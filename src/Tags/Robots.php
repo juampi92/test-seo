3023c4f81b795a1f7c1e7f65425a7cd1ab685591
Juan Pablo Barreto <juampi92@gmail.com>
2022-08-28 15:42:13 +0200

    Robots

---
+++ src/Tags/Robots.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Juampi92\TestSEO\Tags;
+
+use Stringable;
+
+class Robots implements Stringable
+{
+    public const INDEX = 'index';
+
+    public const NO_INDEX = 'noindex';
+
+    public const FOLLOW = 'follow';
+
+    public const NO_FOLLOW = 'nofollow';
+
+    public const NONE = 'none'; // Equivalent to using both the noindex and nofollow tags simultaneously.
+
+    public const NO_IMAGE_INDEX = 'noimageindex';
+
+    public const NO_ARCHIVE = 'noarchive';
+
+    public const NO_CACHE = 'nocache';
+
+    public const NO_SNIPPET = 'nosnippet';
+
+    /** @var array<Robots::*> */
+    private array $parameters;
+
+    public function __construct(
+        string $content
+    ) {
+        $this->parameters = array_map(
+            'trim',
+            explode(',', $content)
+        );
+
+        sort($this->parameters);
+    }
+
+    public function index(): ?bool
+    {
+        if ($this->has(self::INDEX)) {
+            return true;
+        }
+
+        if ($this->has(self::NO_INDEX)) {
+            return false;
+        }
+
+        if ($this->has(self::NONE)) {
+            return false;
+        }
+
+        return null;
+    }
+
+    public function noindex(): ?bool
+    {
+        return $this->negation(
+            $this->index()
+        );
+    }
+
+    public function follow(): ?bool
+    {
+        if ($this->has(self::FOLLOW)) {
+            return true;
+        }
+
+        if ($this->has(self::NO_FOLLOW)) {
+            return false;
+        }
+
+        if ($this->has(self::NONE)) {
+            return false;
+        }
+
+        return null;
+    }
+
+    public function nofollow(): ?bool
+    {
+        return $this->negation($this->follow());
+    }
+
+    public function noarchive(): bool
+    {
+        return $this->has(self::NO_ARCHIVE);
+    }
+
+    public function nocache(): bool
+    {
+        return $this->has(self::NO_CACHE);
+    }
+
+    public function nosnippet(): bool
+    {
+        return $this->has(self::NO_SNIPPET);
+    }
+
+    public function noimageindex(): bool
+    {
+        return $this->has(self::NO_IMAGE_INDEX);
+    }
+
+    /*
+     * Support methods.
+     */
+
+    private function has(string $parameter): bool
+    {
+        return in_array($parameter, $this->parameters);
+    }
+
+    /**
+     * If null, return null.
+     * If boolean, return the negation.
+     */
+    private function negation(?bool $value): ?bool
+    {
+        if (is_null($value)) {
+            return null;
+        }
+
+        return ! $value;
+    }
+
+    public function __toString(): string
+    {
+        return implode(',', $this->parameters);
+    }
+}
---
+++ tests/Tags/RobotsTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Juampi92\TestSEO\Tests\DTOs;
+
+use Juampi92\TestSEO\Tags\Robots;
+use PHPUnit\Framework\TestCase;
+
+class RobotsTest extends TestCase
+{
+    public function test_no_index_no_follow(): void
+    {
+        $robots = new Robots(' noindex , nofollow ');
+
+        $this->assertTrue($robots->noindex());
+        $this->assertFalse($robots->index());
+        $this->assertTrue($robots->nofollow());
+        $this->assertFalse($robots->follow());
+    }
+
+    public function test_none(): void
+    {
+        $robots = new Robots(' none ');
+
+        $this->assertTrue($robots->noindex());
+        $this->assertFalse($robots->index());
+        $this->assertTrue($robots->nofollow());
+        $this->assertFalse($robots->follow());
+    }
+
+    public function test_empty(): void
+    {
+        $robots = new Robots(' invalid ');
+
+        $this->assertNull($robots->noindex());
+        $this->assertNull($robots->index());
+        $this->assertNull($robots->nofollow());
+        $this->assertNull($robots->follow());
+    }
+
+    public function test_others_match(): void
+    {
+        $robots = new Robots('noimageindex,noarchive,nocache,nosnippet');
+
+        $this->assertTrue($robots->noimageindex());
+        $this->assertTrue($robots->noarchive());
+        $this->assertTrue($robots->nocache());
+        $this->assertTrue($robots->nosnippet());
+    }
+
+    public function test_others_dont_match(): void
+    {
+        $robots = new Robots('');
+
+        $this->assertFalse($robots->noimageindex());
+        $this->assertFalse($robots->noarchive());
+        $this->assertFalse($robots->nocache());
+        $this->assertFalse($robots->nosnippet());
+    }
+}
